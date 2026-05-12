262d966a4208be8eab43770d3a35bdfbdf642d28
Hameed <hameed@example.com>
2026-05-12 03:38:04 +0200

    Install React Navigation peer dependencies and wrap App in GestureHandlerRootView

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style="light" />
       <AppNavigator />
-    </>
+    </GestureHandlerRootView>
   );
 }
